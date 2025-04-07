15d07b2504c90a24ae87f6553d500a958eae7e1d
taeyoung <her020212@naver.com>
2025-04-07 14:52:23 +0900

    [04.07] 배열메소드

--- javaScript/01.기초/src/index.js
+++ javaScript/01.기초/src/index.js
@@ -1,7 +1,5 @@
-const arr1 = [1, 2, 3, 4, 5];
-arr1.pop(); //뒤에꺼지우기
-console.log(arr1);
-arr1.shift(); // 앞에꺼 지우기
-// arr1.splice(start, deleteCount);
-arr1.splice(1, 2);
-console.log(arr1);
+let person = {
+  name: "young",
+  age: 18,
+  innerHeight: 200,
+};
